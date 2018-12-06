fe40e96a60ea603d5b55ea635e2dce2e7ef8b5da
brddev <brddev@gmail.com>
2018-12-06 21:25:05 +0100

    Merge branch 'pager_adapter' of https://bitbucket.org/bdushi/calendarview into pager_adapter
    # Conflicts:
    #	calendarview/src/main/java/al/bruno/calendar/view/model/Calendar.java

--- calendarview/src/main/java/al/bruno/calendar/view/CalendarView.java
+++ calendarview/src/main/java/al/bruno/calendar/view/CalendarView.java
@@ -5,12 +5,14 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import org.joda.time.DateTime;
+
 import al.bruno.calendar.view.databinding.CalendarViewBinding;
 import al.bruno.calendar.view.listener.OnDateClickListener;
 import al.bruno.calendar.view.model.Calendar;
 import al.bruno.calendar.view.model.LocalDateTime;
 import androidx.databinding.DataBindingUtil;
-
 public class CalendarView extends LinearLayout implements OnDateClickListener{
 	//event handling
 	private DateTime dateTime = new DateTime();
